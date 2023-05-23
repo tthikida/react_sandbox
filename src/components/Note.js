1646be4b99c058a01d79021ed4d16c5a978b69e6
Tyler <62820237+tthikida@users.noreply.github.com>
2023-05-23 14:15:51 -0700

    add cards and props

--- src/components/Note.js
+++ src/components/Note.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Note() {
     return <div className={"note"}>
-        <h2>Note Title</h2>
+        <h1>Note Title</h1>
         <p>This is a note muthatrucka! Let's get a little mud on the tires.  Cause it's a good night to be out there soakin' up the moonlight...</p>
     </div>
 }
