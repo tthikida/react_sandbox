59625bb61713c3204394be791c931aaf29372273
Tyler <62820237+tthikida@users.noreply.github.com>
2023-05-23 22:18:41 -0700

    adding cards, working on css

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
 function Note() {
-    return <div className={"note"}>
-        <h1>Note Title</h1>
-        <p>This is a note muthatrucka! Let's get a little mud on the tires.  Cause it's a good night to be out there soakin' up the moonlight...</p>
-    </div>
+    return (
+        <div className={"note"}>
+            <h1>Note Title</h1>
+            <p>This is a note muthatrucka! Let's get a little mud on the tires.  Cause it's a good night to be out there soakin' up the moonlight...</p>
+        </div>);
 }
 
 export default Note;
